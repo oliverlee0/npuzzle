6e871fc14e9cda30562925b37e42e0b491b23fe1
Oliver Lee <mainfolder@Olivers-MacBook-Pro-2.local>
2020-10-24 00:44:26 -0700

    Added linear conflicts to heuristic, optimized heuristic by finding the heuristic of a new neighbor through current, and added a pessimistic AStar function that quickly finds a solution (with a lot of moves).

--- puzzle.py
+++ puzzle.py
@@ -1,29 +1,23 @@
+from collections import deque
 import heapq
 
-def storeUnique(C):
+def memoizeClass(C):
 	cache = {}
-	def g(*args):
-		d = args[0]
-		for state in cache:
-			if cache[state] == d: return state
-		s = C(d)
-		cache[s] = d
-		return s
-	return g
+	def constructUnique(*args):
+		s = str(args[0])
+		if s in cache: return cache[s]
+		cache[s] = C(*args)
+		return cache[s]
+	return constructUnique
 
-@storeUnique
+@memoizeClass
 class State:
-	def __init__(self, data):
+	def __init__(self, data, star):
 		self.state = data
+		self.star = star
 		self.rows = len(data)
 		self.cols = len(data[0])
-		for i in range(self.rows):
-			row = self.state[i]
-			if '*' in row: self.star = (i, row.index('*'))
 
-	def value(self, row, col):
-		return self.state[row][col]
-	
 	def pos(self, i):
 		return self.state[i // self.cols][i % self.cols]
 
@@ -34,42 +28,66 @@ class State:
 	def swap(self, row, col):
 		swapState = self.copy()
 		swapState[self.star[0]][self.star[1]] = self.state[row][col]
-		swapState[row][col] = self.state[self.star[0]][self.star[1]]
-		return State(swapState)
+		swapState[row][col] = '*'
+		return State(swapState, (row, col))
 
 	def copy(self):
 		return [row[:] for row in self.state]
-	
-	def equals(self, obj):
-		return self.state == obj.state
 
-	def DebugPrint(self):
-		for row in self.state: print("\t".join(row))
-		print()
-	
+	def __gt__(self, other):
+		return False
+
 	def getGoal(self):
 		goal = []
 		for i in range(self.rows):
 			row = [str(i * self.cols + j + 1) for j in range(self.cols)]
 			goal.append(row)
-		goal[-1][-1] = "*"
-		return State(goal)
+		goal[-1][-1] = '*'
+		return State(goal, (-1, -1))
 		
 	def isGoal(self):
 		for i in range(1, self.rows * self.cols):
 			if self.state[(i - 1) // self.cols][(i - 1) % self.cols] != str(i): return False
 		return True
-	
+		
+	def recursManhattanDistance(self, val):
+		pos = self.index(val)
+		val = int(val)
+		goalRow = (val - 1) // self.cols
+		goalCol = (val - 1) % self.cols
+		newDistance = abs(self.star[0] - goalRow) + abs(self.star[1] - goalCol)
+		oldDistance = abs(pos[0] - goalRow) + abs(pos[1] - goalCol)
+		return newDistance - oldDistance
+
+	def recursManhattanDistance2(self, pos):
+		val = int(self.state[pos[0]][pos[1]])
+		goalRow = (val - 1) // self.cols
+		goalCol = (val - 1) % self.cols
+		newDistance = abs(self.star[0] - goalRow) + abs(self.star[1] - goalCol)
+		oldDistance = abs(pos[0] - goalRow) + abs(pos[1] - goalCol)
+		return newDistance - oldDistance
+
 	def getNeighbors(self):
 		neighbors = []
-		if self.star[0] > 0: neighbors.append((self.star[0] - 1, self.star[1]))
-		if self.star[1] > 0: neighbors.append((self.star[0], self.star[1]  - 1))
 		if self.star[0] < self.rows - 1: neighbors.append((self.star[0] + 1, self.star[1]))
 		if self.star[1] < self.cols - 1: neighbors.append((self.star[0], self.star[1] + 1))
+		if self.star[0] > 0: neighbors.append((self.star[0] - 1, self.star[1]))
+		if self.star[1] > 0: neighbors.append((self.star[0], self.star[1]  - 1))
 		return neighbors
 	
 	def ComputeNeighbors(self):
-		return list(map(lambda pos: (self.value(pos[0], pos[1]), self.swap(pos[0], pos[1])), self.getNeighbors()))
+		return [(self.state[pos[0]][pos[1]], self.swap(pos[0], pos[1])) for pos in self.getNeighbors()]
+
+	def DebugPrint(self):
+		for row in self.state: print("\t".join(row))
+		print()
+
+	def testSwaps(self, l):
+		state = self
+		for v in l:
+			pos = state.index(v)
+			state = state.swap(pos[0], pos[1])
+		return state
 
 # memoizes when state is equal to one in cache
 def memoize(f):
@@ -77,13 +95,14 @@ def memoize(f):
 	def g(x):
 		if x in cache: return cache[x]
 		cache[x] = f(x)
-		return f(x)
+		return cache[x]
 	return g
 
 def isSolvable(state):
 	inversions = 0
-	it = [i for i in range(state.cols * state.rows) if i != state.star[0] * state.cols + state.star[1]]
-	greatest = (int(state.state[it[-1] // state.cols][it[-1] % state.cols]) + 1, 0, -1)
+	omit = state.star[0] * state.cols + state.star[1]
+	it = [i for i in range(omit)] + [i for i in range(omit + 1, state.rows * state.cols)]
+	greatest = (int(state.state[it[-1] // state.cols][it[-1] % state.cols]), 1, -1)
 	for iterator in range(len(it) - 2, -1, -1):
 		num = int(state.state[it[iterator] // state.cols][it[iterator] % state.cols])
 		base = (0, 0, None)
@@ -97,33 +116,30 @@ def isSolvable(state):
 	if state.cols % 2 == 0 and (state.rows - state.star[0]) % 2 == 0:	return inversions % 2 == 1
 	return inversions % 2 == 0			
 
-def swapValues(state, l):
-	newState = state
-	for v in l:
-		pos = newState.index(v)
-		newState = newState.swap(pos[0], pos[1])
-	return newState
-
 def LoadFromFile(filepath):
-	state = []
+	data = []
+	star = ()
 	with open(filepath) as f:
-		for line in f: state.append(line.rstrip('\n').split('\t'))
-	return State(state)
+		for line in f: data.append(line.rstrip('\n').split('\t'))
+	for i in range(len(data)):
+			row = data[i]
+			if '*' in row: star = (i, row.index('*'))
+	return State(data, star)
 
 # checks neighbors in order up, left, down, right
 def BFS(state):
 	if not isSolvable(state) or state.isGoal(): return []
-	frontier, discovered, parents = [], [state], {}
+	frontier, discovered, parents = deque([]), [state], {}
 	for neighbor in state.ComputeNeighbors():
-		frontier = [neighbor[1]] + frontier
+		frontier.append(neighbor[1])
 		discovered.append(neighbor[1])
 		parents[neighbor[1]] = [neighbor[0]]
 	while frontier:
-		current = frontier.pop()
+		current = frontier.popleft()
 		if current.isGoal(): return parents[current]
 		for neighbor in current.ComputeNeighbors():
 			if neighbor[1] not in discovered:
-				frontier = [neighbor[1]] + frontier
+				frontier.append(neighbor[1])
 				discovered.append(neighbor[1])
 				parents[neighbor[1]] = parents[current] + [neighbor[0]]
 
@@ -148,72 +164,173 @@ def DFS(state):
 def BidirectionalSearch(state):
 	if not isSolvable(state): return []
 	goal = state.getGoal()
-	if state.equals(goal): return []
-	frontier, discovered, parents = [], [state], {}
+	if state == goal: return []
+	frontier, discovered, parents = deque([]), [state], {}
 	for neighbor in state.ComputeNeighbors():
-		frontier = [neighbor[1]] + frontier
+		frontier.append(neighbor[1])
 		discovered.append(neighbor[1])
 		parents[neighbor[1]] = [neighbor[0]]
-	frontierG, discoveredG, parentsG = [], [goal], {}
+	frontierG, discoveredG, parentsG = deque([]), [goal], {}
 	for neighborG in goal.ComputeNeighbors():
-		frontierG = [neighborG[1]] + frontierG
+		frontierG.append(neighborG[1])
 		discoveredG.append(neighborG[1])
 		parentsG[neighborG[1]] = [neighborG[0]]
 	while frontier or frontierG:
-		current, currentG = frontier.pop(), frontierG.pop()
+		current, currentG = frontier.popleft(), frontierG.popleft()
 		if current in discoveredG: return parents[current] + parentsG[current]
 		if currentG in discovered: return parents[currentG] + parentsG[currentG]
 		for neighbor in current.ComputeNeighbors():
 			if neighbor[1] not in discovered:
-				frontier = [neighbor[1]] + frontier
+				frontier.append(neighbor[1])
 				discovered.append(neighbor[1])
 				parents[neighbor[1]] = parents[current] + [neighbor[0]]
 		for neighborG in currentG.ComputeNeighbors():
 			if neighborG[1] not in discoveredG:
-				frontierG = [neighborG[1]] + frontierG
+				frontierG.append(neighborG[1])
 				discoveredG.append(neighborG[1])
 				parentsG[neighborG[1]] = [neighborG[0]] + parentsG[currentG]
 
+def manhattanDistance(state):
+	dis = 0
+	data = state.copy()
+	data[state.star[0]][state.star[1]] = 0
+	data = [[int(item) for item in row] for row in data]
+	for row in range(state.rows):
+		for col in range(state.cols):
+			val = data[row][col]
+			if val:
+				correctPos = ((val - 1) // state.cols, (val - 1) % state.cols)
+				dis += abs(correctPos[0] - row) + abs(correctPos[1] - col)
+	return dis
+
+def getConflictTiles(state):
+	data = state.copy()
+	data[state.star[0]][state.star[1]] = 0
+	data = [[int(item) for item in row] for row in data]
+	rowConflicts, colConflicts = {}, {}
+	rowConflictTiles, colConflictTiles = set(), set()
+	for row in range(len(data)):
+		conflictsHeap = []
+		for tile in range(len(data[0])):
+			if data[row][tile]:
+				rowConflicts[(tile, row)] = findRowConflict(data, tile, row)
+				heapq.heappush(conflictsHeap, (-len(rowConflicts[(tile, row)]), tile))
+		while any([rowConflicts[key] for key in rowConflicts if key[1] == row]):
+			biggestConflictTile = heapq.heappop(conflictsHeap)[1]
+			rowConflicts[(biggestConflictTile, row)] = 0
+			for t in conflictsHeap:
+				if biggestConflictTile in rowConflicts[(t[1], row)]:
+					rowConflicts[(t[1], row)] = [item for item in rowConflicts[(t[1], row)] if item != biggestConflictTile]
+			rowConflictTiles.add(str(data[row][biggestConflictTile]))
+	for col in range(len(data[0])):
+		conflictsHeap = []
+		for tile in range(len(data)):
+			if data[tile][col]:
+				colConflicts[(tile, col)] = findColConflict(data, tile, col)
+				heapq.heappush(conflictsHeap, (-len(colConflicts[(tile, col)]), tile))
+		while any([colConflicts[key] for key in colConflicts if key[1] == col]):
+			biggestConflictTile = heapq.heappop(conflictsHeap)[1]
+			colConflicts[(biggestConflictTile, col)] = 0
+			for t in conflictsHeap:
+				if biggestConflictTile in colConflicts[(t[1], col)]:
+					colConflicts[(t[1], col)] = [item for item in colConflicts[(t[1], col)] if item != biggestConflictTile]
+			colConflictTiles.add(str(data[biggestConflictTile][col]))
+	return (rowConflictTiles, colConflictTiles, 2 * (len(rowConflictTiles) + len(colConflictTiles)))
+
+def findRowConflict(data, tile, row):
+	val = data[row][tile]
+	if (val - 1) // len(data[0]) != row: return []
+	correctCol = (val - 1) % len(data[0])
+	if correctCol == tile: return []
+	conflictCols = []
+	if correctCol > tile:
+		for i in range(tile + 1, correctCol + 1):
+			if (data[row][i] - 1) // len(data[0]) == row: conflictCols.append(i)
+	for i in range(correctCol, tile):
+		if (data[row][i] - 1) // len(data[0]) == row: conflictCols.append(i)
+	return conflictCols
+
+def findColConflict(data, tile, col):
+	val = data[tile][col]
+	if (val - 1) % len(data[0]) != col: return []
+	correctRow = (val - 1) // len(data[0])
+	if correctRow == tile: return []
+	conflictRows = []
+	if correctRow > tile:
+		for i in range(tile + 1, correctRow + 1):
+			if (data[i][col] - 1) % len(data[0]) == col: conflictRows.append(i)
+	for i in range(correctRow, tile):
+		if (data[i][col] - 1) % len(data[0]) == col: conflictRows.append(i)
+	return conflictRows
+
+def lastMove(state):
+	if str(state.rows * state.cols - 1) not in state.state[-1] and str(state.cols * (state.rows - 1)) not in [row[-1] for row in state.state]: return 2
+	return 0
+
 def AStar(state):
 	if not isSolvable(state) or state.isGoal(): return []
-	count = 0
 	frontier, discovered, parents = [], [state], {}
+	conflictTiles = getConflictTiles(state)
+	prevCost = manhattanDistance(state) + conflictTiles[2]
 	for neighbor in state.ComputeNeighbors():
-		t = (totalDisplacement(neighbor[1]), count, neighbor[1])
-		heapq.heappush(frontier, t)
 		discovered.append(neighbor[1])
 		parents[neighbor[1]] = [neighbor[0]]
-		count += 1
+		cost = prevCost + 1 + state.recursManhattanDistance(neighbor[0])
+		if neighbor[0] in conflictTiles[0]:	cost -= 2
+		if neighbor[0] in conflictTiles[1]:	cost -= 2
+		t = (cost, neighbor[1])
+		heapq.heappush(frontier, t)
 	while frontier:
 		current = heapq.heappop(frontier)
-		if current[2].isGoal():
-			return parents[current[2]]
-		for neighbor in current[2].ComputeNeighbors():
+		prevCost = current[0]
+		print(prevCost)
+		current = current[1]
+		if current.isGoal():
+			return parents[current]
+		for neighbor in current.ComputeNeighbors():
 			if neighbor[1] not in discovered:
-				t = (current[0] + totalDisplacement(neighbor[1]), count, neighbor[1])
-				heapq.heappush(frontier, t)
 				discovered.append(neighbor[1])
-				parents[neighbor[1]] = parents[current[2]] + [neighbor[0]]
-				count += 1
-	
-@memoize
-def totalDisplacement(state):
-	dis = 0
-	omit = state.star[0] * state.cols + state.star[1]
-	for i in range(omit):
-		val = int(state.state[i // state.cols][i % state.cols])
-		correctPos = ((val - 1) // state.cols, (val - 1) % state.cols)
-		dis += abs(correctPos[0] - (i // state.cols)) + abs(correctPos[1] - (i % state.cols))
-	dis += abs(state.rows - state.star[0] - 1) + abs(state.cols - state.star[1] - 1)
-	for i in range(omit + 1, state.cols * state.rows):
-		val = int(state.state[i // state.cols][i % state.cols])
-		correctPos = ((val - 1) // state.cols, (val - 1) % state.cols)
-		dis += abs(correctPos[0] - (i // state.cols)) + abs(correctPos[1] - (i % state.cols))
-	return dis
+				parents[neighbor[1]] = parents[current] + [neighbor[0]]
+				cost = prevCost + 1 + current.recursManhattanDistance(neighbor[0])
+				if neighbor[0] not in parents[current]:
+					if neighbor[0] in conflictTiles[0]: cost -= 2
+					if neighbor[0] in conflictTiles[1]: cost -= 2
+				t = (cost, neighbor[1])
+				heapq.heappush(frontier, t)
+
+#AStar but not admissible
+def PessimisticAStar(state):
+	if not isSolvable(state) or state.isGoal(): return []
+	frontier, discovered, parents = [], [state], {}
+	conflictTiles = getConflictTiles(state)
+	for neighbor in state.ComputeNeighbors():
+		discovered.append(neighbor[1])
+		parents[neighbor[1]] = [neighbor[0]]
+		cost = state.recursManhattanDistance(neighbor[0])
+		if neighbor[0] in conflictTiles[0]:	cost -= 2
+		if neighbor[0] in conflictTiles[1]:	cost -= 2
+		t = (cost, neighbor[1])
+		heapq.heappush(frontier, t)
+	while frontier:
+		current = heapq.heappop(frontier)
+		prevCost = current[0]
+		current = current[1]
+		if current.isGoal():
+			return parents[current]
+		for neighbor in current.ComputeNeighbors():
+			if neighbor[1] not in discovered:
+				discovered.append(neighbor[1])
+				parents[neighbor[1]] = parents[current] + [neighbor[0]]
+				cost = prevCost + current.recursManhattanDistance(neighbor[0])
+				if neighbor[0] not in parents[current]:
+					if neighbor[0] in conflictTiles[0]: cost -= 2
+					if neighbor[0] in conflictTiles[1]: cost -= 2
+				t = (cost, neighbor[1])
+				heapq.heappush(frontier, t)
 
 def main():
 	state = LoadFromFile('game.txt')
-	f = BFS
+	f = AStar
 	print(str(f.__name__) + ": " + (str(f(state))))
 
 if __name__ == '__main__':
